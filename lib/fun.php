34eefc6d6068620cde8197bcfb96e1d8e4d02dcd
jerry <jerry@gmail.com>
2018-10-01 17:26:12 +0800

    把上传的功能封装到fun中

--- lib/fun.php
+++ lib/fun.php
@@ -43,4 +43,83 @@ function msg($type,$msg=null,$url=null)
 	exit;
 }
 
+/*
+* 图像上传
+* @param $file
+* @return string
+*/
+
+function imgUpload($file)
+{	
+	//检查上传文件是否合法
+	if(!is_uploaded_file($file['tmp_name']))
+	{
+		msg(2,'请上传符合规范的图片');
+	}
+	
+	$type = $file['type'];
+	
+	if(!in_array($type,array("image/png","image/gif","image/jpeg")))
+	{
+		msg(2,'请上传png,git,jpg图像');
+	}
+	
+	
+	//上传目录
+	$uploadPath = './static/file/';
+	//上传目录访问url
+	$uploadUrl = '/static/file/';
+	//上传文件夹
+	$fileDir = date('Y/md/',$_SERVER['REQUEST_TIME']);
+	
+	//检查上传目录是否存在
+	if(!is_dir($uploadPath.$fileDir))
+	{
+		mkdir($uploadPath.$fileDir,0755,true);//递归创建目录
+	}
+	//文件名转换为小写
+	$ext = strtolower(pathinfo($file['name'],PATHINFO_EXTENSION));
+	
+	//上传图像名称
+	$img = uniqid().mt_rand(1000,9999).'.'.$ext;
+	//物理地址
+	$imgPath = $uploadPath.$fileDir.$img;
+	//URL地址
+	$imgUrl = 'http://127.0.0.1'.$uploadUrl.$fileDir.$img;
+	
+	//var_dump($imgPath,$imgUrl);
+	
+	//var_dump($imgUrl);
+	if(!move_uploaded_file($file['tmp_name'],$imgPath))
+	{
+		msg(2,'服务器繁忙，请稍后再试');
+	}
+	
+	return $imgUrl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- publish.php
+++ publish.php
@@ -20,52 +20,10 @@ if(!empty($_POST['name']))
 	$file = $_FILES['file'];
 	$now = $_SERVER['REQUEST_TIME'];
 	
+	$img = imgUpload($file);
+
 	
-	//var_dump($file);die;
-	
-	//检查上传文件是否合法
-	if(!is_uploaded_file($file['tmp_name']))
-	{
-		msg(2,'请上传符合规范的图片');
-	}
-	
-	$type = $file['type'];
-	
-	if(!in_array($type,array("image/png","image/gif","image/jpeg")))
-	{
-		msg(2,'请上传png,git,jpg图像');
-	}
-	
-	
-	//上传目录
-	$uploadPath = './static/file/';
-	//上传目录访问url
-	$uploadUrl = '/static/file/';
-	//上传文件夹
-	$fileDir = date('Y/md/',$now);
-	
-	//检查上传目录是否存在
-	if(!is_dir($uploadPath.$fileDir))
-	{
-		mkdir($uploadPath.$fileDir,0755,true);//递归创建目录
-	}
-	//文件名转换为小写
-	$ext = strtolower(pathinfo($file['name'],PATHINFO_EXTENSION));
-	
-	//上传图像名称
-	$img = uniqid().mt_rand(1000,9999).'.'.$ext;
-	//物理地址
-	$imgPath = $uploadPath.$fileDir.$img;
-	//URL地址
-	$imgUrl = 'http://127.0.0.1'.$uploadUrl.$fileDir.$img;
-	
-	//var_dump($imgPath,$imgUrl);
-	
-	//var_dump($imgUrl);
-	if(move_uploaded_file($file['tmp_name'],$imgPath))
-	{
-		
-	}
+	echo $img;die;
 	
 } 
 
